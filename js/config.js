8cee7fcec9ba66a4668b1dd6b994818275e1c0dd
Ragnar242 <103238481+Ragnar242@users.noreply.github.com>
2024-12-09 15:22:23 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_O0ksp2b1G', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'i0o686tnvd055ubsroe0slse4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Y61fbDHXs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7om46kqrkrnp1kut0auced0lpv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
